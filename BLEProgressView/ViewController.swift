d08ff9d45c78765d08c965e4b4ceef9722f56e2a
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2018-01-25 03:41:32 +0900

    Initial Commit

---
+++ BLEProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEProgressView
+//
+//  Created by Shinya Yamamoto on 2018/01/25.
+//  Copyright © 2018年 Shinya Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
